9e802223748394d9270541b3fd29667dacebc512
Lyuha <110347571+Lyuhamin@users.noreply.github.com>
2022-12-14 17:21:16 +0900

    practice

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const days0fweek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get item from Array
+console.log(days0fweek);
+
+//add one more day to the array 
+days0fweek.push("sun");
+
+console.log(days0fweek);
