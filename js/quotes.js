b7c2aabad9ff579eed80b2383b3fd4727799a746
Lyuha <110347571+Lyuhamin@users.noreply.github.com>
2022-12-28 19:01:57 +0900

    quote

---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+const quote = [
+{
+    quote: "A living dog is better than a dead lion",
+    author: "WooWakGood",
+},
+{
+    quote: "Break and break! If you put everything down and admit it, you'll grow that much",
+    author: "WooWakGood",
+},
+{
+    quote: "Every man has business and desire, Such as it is.",
+    author: "William Shakespeare",
+},
+{
+    quote: "All would live long, but none would be old.",
+    author: "Benjamin Franklin",
+},
+{
+    quote: "You are built not to shrink down to less but to blossom into more.",
+    author: "Oprah Winfrey",
+},
+{
+    quote: "You know, my faith is one that admits some doubt.",
+    author: "Barack Obama",
+},
+{
+    quote: "As you grow older, you'll find the only things you regret are the things you didn't do.",
+    author: "Zachary Scott",
+},
+{
+    quote: "The quickest way of ending a war is to lose it.",
+    author: "George Orwell",
+},
+{
+    quote: "Nothing is as far away as one minute ago.",
+    author: "Jim Bishop",
+},
+{
+    quote: "We can draw lessons from the past, but we cannot live in it.",
+    author: "Lyndon B. Johnson",
+},
+]
