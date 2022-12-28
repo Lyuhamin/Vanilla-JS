08d0063bf96d880a279ceb150e18252e031f86c6
Lyuha <110347571+Lyuhamin@users.noreply.github.com>
2022-12-28 18:43:47 +0900

    plus

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
