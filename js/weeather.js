c0c631edeba9aa6ffd5df7293c77609fd1e609d4
Lyuha <110347571+Lyuhamin@users.noreply.github.com>
2023-01-07 16:47:35 +0900

    Complete

--- js/weeather.js
+++ js/weeather.js
@@ -1,7 +1,16 @@
+const API_KEY = "213d30af69c2218534661eefd5e0788a";
+
 function onGeoOk(posotion) {
     const lat = posotion.coords.latitude;
-    const lng = posotion.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = posotion.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
     
 }
 function onGeoError() {
